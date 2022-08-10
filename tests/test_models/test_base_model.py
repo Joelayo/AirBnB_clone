feea14febf87f724fb05c7da2e357b91eda7a937
Joelayo <joetech.ooj@gmail.com>
2022-08-10 07:16:12 +0100

    [Fix] - Conflict

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -76,8 +76,8 @@ class TestBaseModel(unittest.TestCase):
     def test_3_id(self):
         """Tests for unique user ids."""
 
-        value = [BaseModel().id for i in range(1000)]
-        self.assertEqual(len(set(value)), len(value))
+        l = [BaseModel().id for i in range(1000)]
+        self.assertEqual(len(set(l)), len(l))
 
     def test_3_save(self):
         """Tests the public instance method save()."""
