27b08a3c4e00ed9757f77ae4aa4de1a17a15b40d
Favvie <favourprecious3@gmail.com>
2022-08-10 04:08:26 +0100

    add test base

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,3 @@
-#!/usr/bin/python3
-""" init models """
-
 from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
--- models/base_model.py
+++ models/base_model.py
@@ -1,46 +1,47 @@
 #!/usr/bin/python3
-"""This module contains a class called BaseModel"""
+"""File containing BaseModel Class"""
 
-
+from datetime import datetime
 import models
 import uuid
-from datetime import datetime
 
 
 class BaseModel:
-    """ a class called basemodel used to create a basemodel """
-    def __init__(self, *args, **kwargs):
-        """this initialization method adds an id, and created_at attributes"""
 
-        if len(kwargs) == 0:
+    def __init__(self, *args, **kwargs):
+        """Initialize a new Base Model"""
+        if kwargs is None or len(kwargs) == 0:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
             self.updated_at = datetime.utcnow()
             models.storage.new(self)
-
         else:
             for key, value in kwargs.items():
-                if key == "__class__":
-                    continue
-                elif key == "created_at" or key == "updated_at":
-                    setattr(self, key, datetime.fromisoformat(value))
+                if key in ["created_at", "updated_at"]:
+                    self.__dict__[key] = datetime\
+                        .strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+
+                elif key == "id":
+                    self.id = value
+                elif key == "__class__":
+                    self.__class__.__name__ = kwargs[key]
                 else:
                     setattr(self, key, value)
 
     def __str__(self):
-        """This method overwrites the default str method"""
-        return "[{}] ({}) {}"\
-            .format(self.__class__.__name__, self.id, self.__dict__)
+        """String representation of the Base Model"""
+        classname = self.__class__.__name__
+        return "[{}] ({}) {}".format(classname, self.id, self.__dict__)
 
     def save(self):
-        """This method updates the updated attribute to the current time"""
+        """Updates updated_at with the current time"""
         self.updated_at = datetime.utcnow()
         models.storage.save()
 
     def to_dict(self):
-        """This method adds the classname as a key to the instance """
-        obj_dict = self.__dict__.copy()
-        obj_dict["created_at"] = datetime.isoformat(self.created_at)
-        obj_dict["updated_at"] = datetime.isoformat(self.updated_at)
-        obj_dict["__class__"]: self.__class__.__name__
-        return obj_dict
+        """Creates a dictionary of BaseModel"""
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = self.__class__.__name__
+        my_dict["created_at"] = datetime.isoformat(self.created_at)
+        my_dict["updated_at"] = datetime.isoformat(self.updated_at)
+        return my_dict
